d3166dcbf13818d1f5051e76189ddb0b6ac1e362
João Augusto <joaoalt0502@gmail.com>
2025-01-13 12:07:29 -0300

    spring

---
+++ src/main/java/br/com/gps/mecanica/MecanicaFxMainApplication.java
@@ -0,0 +1,31 @@
+package br.com.gps.mecanica;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.context.ConfigurableApplicationContext;
+
+import javafx.application.Application;
+import javafx.application.Platform;
+import javafx.stage.Stage;
+
+public class MecanicaFxMainApplication extends Application {
+
+    private ConfigurableApplicationContext applicationContext;
+
+    @Override
+    public void init() {
+    applicationContext =
+        new SpringApplicationBuilder(MecanicaApplication.class)
+            .run(getParameters().getRaw().toArray(new String[0]));
+    }
+
+    @Override
+    public void start(Stage stage) {
+        applicationContext.publishEvent(new StageReadyEvent(stage));
+    }
+
+    @Override
+    public void stop() {
+        applicationContext.close();
+        Platform.exit();
+    }
+}
---
+++ src/main/java/br/com/gps/mecanica/StageReadyEventListener.java
@@ -0,0 +1,42 @@
+package br.com.gps.mecanica;
+
+import java.io.IOException;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.ApplicationListener;
+import org.springframework.stereotype.Component;
+
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import lombok.RequiredArgsConstructor;
+
+@Component
+@RequiredArgsConstructor
+public class StageReadyEventListener implements ApplicationListener<StageReadyEvent> {
+
+    @Value("Sistema para Oficina Mecanica")
+    private String windowTitle;
+
+    private Scene scene;
+
+    @SuppressWarnings("null")
+    @Override
+    public void onApplicationEvent(StageReadyEvent event) {
+        var stage = event.getStage();
+        try {
+            scene = new Scene(loadFXML("main"), 640, 480);
+        } catch (IOException e) {
+            System.out.println("Erro ao carregar o arquivo FXML: " + e.getMessage());
+            e.printStackTrace();
+        }
+        stage.setScene(scene);
+        stage.setTitle("Sistema para Oficina Mecanica");
+        stage.show();
+    }
+
+    private static Parent loadFXML(String fxml) throws IOException {
+        FXMLLoader fxmlLoader = new FXMLLoader(MecanicaApplication.class.getResource(fxml + ".fxml"));
+        return fxmlLoader.load();
+    }
+}
