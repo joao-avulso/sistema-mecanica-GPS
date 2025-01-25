1f32216c7d2e37c5ab66eccb01e7c37655170103
lucas-guerhardt <lucaspin03@hotmail.com>
2025-01-25 15:20:34 -0300

    Adicionado Odem Servico

---
+++ src/main/java/br/com/gps/mecanica/models/OrdemServicoModel.java
@@ -0,0 +1,139 @@
+package br.com.gps.mecanica.models;
+
+import java.io.Serial;
+import java.io.Serializable;
+import java.util.Set;
+import java.util.UUID;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.JoinTable;
+import jakarta.persistence.ManyToMany;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "ordens_servico")
+public class OrdemServicoModel implements Serializable {
+    @Serial
+    private static final long serialVersionUID = 1L;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private UUID id;
+
+    @ManyToOne
+    @JoinColumn(name = "idCliente")
+    private ClienteModel cliente;
+
+    @ManyToOne
+    @JoinColumn(name = "idVeiculo")
+    private VeiculoModel veiculo;
+
+    @ManyToMany
+    @JoinTable(name = "servicos_ordem_servico", joinColumns = @JoinColumn(name = "idOrdemServico"), inverseJoinColumns = @JoinColumn(name = "idServico"))
+    private Set<ServicoModel> servicos;
+
+    @ManyToMany
+    @JoinTable(name = "produtos_ordem_servico", joinColumns = @JoinColumn(name = "idOrdemServico"), inverseJoinColumns = @JoinColumn(name = "idProdutos"))
+    private Set<ProdutoModel> produtos;
+
+    @ManyToOne
+    @JoinColumn(name = "idFuncionario")
+    private FuncionarioModel funcionario;
+
+    private String descricao;
+
+    public OrdemServicoModel() {
+    }
+
+    public OrdemServicoModel(ClienteModel cliente, VeiculoModel veiculo, Set<ServicoModel> servicos,
+            Set<ProdutoModel> produtos, FuncionarioModel funcionario, String descricao) {
+        this.cliente = cliente;
+        this.veiculo = veiculo;
+        this.servicos = servicos;
+        this.produtos = produtos;
+        this.funcionario = funcionario;
+        this.descricao = descricao;
+    }
+
+    public UUID getId() {
+        return id;
+    }
+
+    public void setId(UUID id) {
+        this.id = id;
+    }
+
+    public ClienteModel getCliente() {
+        return cliente;
+    }
+
+    public void setCliente(ClienteModel cliente) {
+        this.cliente = cliente;
+    }
+
+    public VeiculoModel getVeiculo() {
+        return veiculo;
+    }
+
+    public void setVeiculo(VeiculoModel veiculo) {
+        this.veiculo = veiculo;
+    }
+
+    public Set<ServicoModel> getServicos() {
+        return servicos;
+    }
+
+    public void setServicos(Set<ServicoModel> servicos) {
+        this.servicos = servicos;
+    }
+
+    public Set<ProdutoModel> getProdutos() {
+        return produtos;
+    }
+
+    public void setProdutos(Set<ProdutoModel> produtos) {
+        this.produtos = produtos;
+    }
+
+    public FuncionarioModel getFuncionario() {
+        return funcionario;
+    }
+
+    public void setFuncionario(FuncionarioModel funcionario) {
+        this.funcionario = funcionario;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+
+    @Override
+    public String toString() {
+        String produtosString = "";
+        for (ProdutoModel produto : produtos) {
+            produtosString += produto.toString();
+            produtosString += ", ";
+        }
+
+        String servicosString = "";
+        for (ServicoModel servico : servicos) {
+            servicosString += servico.toString();
+            servicosString += ", ";
+        }
+        return "OrdemServicoModel [cliente=" + cliente.toString() + ", descricao=" + descricao + ", funcionario="
+                + funcionario
+                + ", id=" + id + ", produtos=" + produtosString + ", servicos=" + servicosString
+                + ", veiculo=" + veiculo.toString()
+                + "]";
+    }
+
+}
