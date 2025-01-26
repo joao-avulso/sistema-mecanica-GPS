245f6b2c86b5a88058bc31291bf609f05bc0f1ba
Aaahhv <aaahhv2@gmail.com>
2025-01-26 20:41:59 -0300

    tem que adicionar mais algumas metodos de teste, mas por em quanto é suficiente

--- src/test/java/br/com/gps/mecanica/services/ProdutoServiceTest.java
+++ src/test/java/br/com/gps/mecanica/services/ProdutoServiceTest.java
@@ -1,13 +1,18 @@
 package br.com.gps.mecanica.services;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.fail;
+
+import java.util.ArrayList;
 import java.util.List;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.transaction.annotation.Transactional;
- 
+
+import br.com.gps.mecanica.enums.PessoaEnum;
+import br.com.gps.mecanica.models.FornecedorModel;
 import br.com.gps.mecanica.models.ProdutoModel;
 import br.com.gps.mecanica.repositories.ProdutoRepository;
 
@@ -51,5 +56,66 @@ public class ProdutoServiceTest {
         //Testa o retorno vazio
         List<ProdutoModel> result1 = produtoService.get();
         assertEquals(2, result1.size());
+    }
+
+    //TESTAR OS OUTROS GETs, UPDATE
+
+    @Test
+    void testCreate() {
+
+        ProdutoModel produto1 = new ProdutoModel(
+            "Óleo de Motor",
+            "Óleo sintético para motores a gasolina",
+            50.0,
+            35.0,
+            200,
+            null);
+            
+        try {
+            ProdutoModel result = produtoService.create(produto1);
+
+            assertEquals("OLEO DE MOTOR", produtoService.get(result.getId()).getNome());  
+
+        } catch (Exception e) {
+            fail("Nao foi possivel criar o fornecedor no testCreate do FornecedorServiceTest");
+        }
+
+    }
+
+    @Test
+    void testUpdate() {
+        
+
+        ProdutoModel produto1 = new ProdutoModel(
+            "Óleo de Motor",
+            "Óleo sintético para motores a gasolina",
+            50.0,
+            35.0,
+            200,
+            null);
+
+        //FORNECEDOR
+        FornecedorModel fornecedor = new FornecedorModel(PessoaEnum.JURIDICA, "Alvo Dumbledor","dumbledore@gmail.com",
+        new ArrayList<>() ,new ArrayList<>(),"36.578.284/0001-45",new ArrayList<>());
+            
+        ProdutoModel produto2 = new ProdutoModel(
+            "Filtro de Ar",
+            "Filtro de ar de alta eficiência para veículos compactos",
+            30.0,
+            20.0,
+            150,
+            fornecedor);
+            
+        try {
+            ProdutoModel result = produtoService.create(produto1);
+            assertEquals("OLEO DE MOTOR", produtoService.get(result.getId()).getNome());  
+            
+            ProdutoModel result2 = produtoService.update(produto1.getId(), produto2);
+            assertEquals("FILTRO DE AR", produtoService.get(result2.getId()).getNome());  
+
+        } catch (Exception e) {
+            fail("Nao foi possivel criar o fornecedor no testCreate do FornecedorServiceTest");
+        }
+
     }
 }
